aa9ca28e7970abfebac9892af5f9cf8f36c83cef
Chang_Gao <gaochangw@outlook.com>
2023-02-19 16:50:00 +0100

    add cen

--- ram_ctrl/ram_sp.sv
+++ ram_ctrl/ram_sp.sv
@@ -2,19 +2,20 @@ module ram_sp #(
   parameter WIDTH = 8,
   parameter DEPTH = 256
 ) (
-  input wire             clk,
-  input wire [WIDTH-1:0] din,
-  input wire [7:0]       addr,
-  input wire             wen,  // Active Low
-  output reg [WIDTH-1:0] dout
+    input wire                     clk,
+    input wire [WIDTH-1:0]         din,
+    input wire [$clog2(DEPTH)-1:0] addr,
+    input wire                     cen,  // Active Low
+    input wire                     wen,  // Active Low
+    output reg [WIDTH-1:0]         dout
 );
-  reg [WIDTH-1:0] mem [0:DEPTH-1];
+    reg [WIDTH-1:0] mem [DEPTH-1:0];
 
-  always @(posedge clk) begin
-    if (!wen) begin
-      mem[addr] <= din;
+    always @(posedge clk) begin
+        if (!wen & !cen) begin
+            mem[addr] <= din;
+        end
     end
-  end
 
-  assign dout = mem[addr];
+    assign dout = mem[addr];
 endmodule
